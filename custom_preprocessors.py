7fce561facfb03a0dc54971b26128cfe384f70e6
UserKrzysztof <krzysiektkaczyk02@gmail.com>
2024-05-10 23:17:42 +0200

    add custom_preprocessors.py, new transformer for genres and director_professions, tested the transformer

---
+++ custom_preprocessors.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class OneHotEncoderForMultiStrFeature(BaseEstimator, TransformerMixin):
+    def __init__(self, str_feature_name: str, delimiter: str = ",", skip_values: list = [] ) -> None:
+        super().__init__()
+        self.str_feature_name = str_feature_name
+        self.delimiter = delimiter
+        self.skip_values = skip_values
+
+    def _create_binary_features_from_str_feature(self, row: pd.Series) -> pd.Series:
+        values = row[self.str_feature_name].split(self.delimiter)
+        for v in values:
+            if v in self.skip_values: continue
+            row['is_{0}_{1}'.format(*[self.str_feature_name,v])] = 1
+        return row
+
+    def one_hot_encoding_for_multi_str_feature(self, data_frame: pd.DataFrame) -> pd.DataFrame:
+        df = data_frame.apply(lambda row: self._create_binary_features_from_str_feature(row), 
+                              axis=1).drop(columns = self.str_feature_name)
+        colnames = df.filter(regex=f"is_{self.str_feature_name}").columns.values.tolist()
+        df[colnames]= df[colnames].fillna(0).astype(np.int64)
+        return df
+
+    def fit(self,X, y=None):
+        return self
+    
+    def transform(self,X):
+        return self.one_hot_encoding_for_multi_str_feature(X)
+
